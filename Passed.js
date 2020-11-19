87036708663e9abceea9d6c2983d31fa51793570
nathan olenik <nolenik23@outlook.com>
2020-11-18 20:36:28 -0500

    added more tasks

---
+++ Passed.js
@@ -0,0 +1,16 @@
+
+function myFunction(num){
+return 5 + num
+};
+// console.log(myFunction(56));
+// console.log(myFunction(17));
+// console.log(myFunction(44));
+// var x =5;
+// console.log(x);
+
+function secondFunction(array){
+for(var i = 0; i<array.length;i++){
+    console.log(array[i])
+}
+}
+secondFunction(["cow","chicken","goat"]);
