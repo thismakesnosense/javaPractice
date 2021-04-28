456f7b4f891b3df7379a1dca00d87bbbe25f2dee
nathan olenik <nolenik23@outlook.com>
2021-04-27 20:14:21 -0400

    lotto and finish clock

--- Clock/main.js
+++ Clock/main.js
@@ -5,17 +5,17 @@ var Second = document.querySelector(".Second");
 function setDate(){
 const now = new Date();
 const Seconds = now.getSeconds();
-const SecondsDegree = (Seconds / 60) * 360 + 90;
+const SecondsDegree = (Seconds / 60) * 360 - 90;
 
 Second.style.transform = `rotate(${SecondsDegree}deg)`;
 
 const Hours = now.getHours();
-const HoursDegree = (Hours / 12) * 360 + 90;
+const HoursDegree = (Hours / 12) * 360 - 90;
 
 Hour.style.transform = `rotate(${HoursDegree}deg)`;
 
 const Minutes = now.getMinutes();
-const MinutesDegree = (Minutes / 60) * 360 + 90;
+const MinutesDegree = (Minutes / 60) * 360 - 90;
 
 Minute.style.transform = `rotate(${MinutesDegree}deg)`;
 
--- LotteryGen/main.js
+++ LotteryGen/main.js
@@ -1,13 +1,24 @@
+const winningNum = document.querySelector(".winning-num");
+const userNum =  document.querySelector(".user-num");
+const userInputChoice = document.querySelector(".user-choice");
 
 document.body.onload = () => {
  document.querySelector(".play-form").addEventListener("click", (event) => {
   event.preventDefault();
-  const userChoice = document.querySelector(".user-choice").value;
+  const userChoice = userInputChoice.value;
+  if (userChoice.length !== 4){
+      alert("Please enter a four digit number.")
+      return
+  }; 
+  
   console.log(userChoice);
-  console.log(generateWinningNumber());
+  const winNum = generateWinningNumber();
+  winningNum.innerText=winNum;
+  userNum.innerText=userChoice;
+  userInputChoice.value = "";
  })
 }
-
+// make helper function to check is user number matches winning number then set prizes for how many numbers match 
 const generateWinningNumber = () => Math.floor(Math.random()*9000+1000); 
 
- 
+
