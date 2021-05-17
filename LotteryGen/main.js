1e9e479493ca25205318a0de6e4b83105d473be3
nathan olenik <nolenik23@outlook.com>
2021-05-16 21:40:29 -0400

    added color scheme

--- LotteryGen/main.js
+++ LotteryGen/main.js
@@ -1,6 +1,7 @@
 const winningNum = document.querySelector(".winning-num");
 const userNum =  document.querySelector(".user-num");
 const userInputChoice = document.querySelector(".user-choice");
+const lightShow = document.querySelector(".wow");
 
 document.body.onload = () => {
  document.querySelector(".play-form").addEventListener("click", (event) => {
@@ -18,6 +19,16 @@ document.body.onload = () => {
   userInputChoice.value = "";
  })
 }
+var j=0;
+function changeColors(){
+var headTag = lightShow;
+var bgColor = ["red", "yellow", "blue"];
+headTag.style.backgroundColor = bgColor[j];
+j=(j+1)%bgColor.length;
+//  lightShow.append('Win');
+};
+// function above and var j sets and array of colors for the background color to change to then setting interval allows it to change with the two peramiters. 
+setInterval(changeColors, 2000);
 // make helper function to check is user number matches winning number then set prizes for how many numbers match 
 const generateWinningNumber = () => Math.floor(Math.random()*9000+1000); 
 
