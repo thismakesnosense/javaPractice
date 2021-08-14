53c43134b04891c4450cc6db5752f3b7af4c7e98
nathan olenik <nolenik23@outlook.com>
2021-08-13 20:08:26 -0400

    fiveday

--- WeatherApp/public/script.js
+++ WeatherApp/public/script.js
@@ -2,34 +2,59 @@
 const searchInput = document.querySelector(".search-tap");
 const searchBtn = document.querySelector(".search-btn");
 
+
 // helper functions
 
 const fetchFiveDay = () => {
-fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchInput.value}&units=imperial&appid=`)
- .then(res => res.json())
- .then(data => {
-    //  console.log("data", data);
-     formatData(data.list);
- });
-
+    fetch('/fiveday', {
+        method:"POST",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body:JSON.stringify({
+            city: searchInput.value
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    })
 }
 
-const formatData = (weatherArray) => {
-const results = [];
-for (let i = 4; i < weatherArray.length; i+=8){
-    const current = weatherArray[i];
-    const weatherOBJ = {
-        temp: current.main.temp,
-        precipitation: current.weather[0].description,
-        icon: `http://openweathermap.org/img/w/${current.weather[0].icon}.png`,
-        date: new Date(current.dt*1000),
-        wind: current.wind.speed,
-    }
-    results.push(weatherOBJ);
-}
-console.log(results);
-return results;
-}; 
+//this formatting was moved to server.js
+// const formatData = (weatherArray) => {
+// const results = [];
+// for (let i = 4; i < weatherArray.length; i+=8){
+//     const current = weatherArray[i];
+//     const weatherOBJ = {
+//         temp: current.main.temp,
+//         precipitation: current.weather[0].description,
+//         icon: `http://openweathermap.org/img/w/${current.weather[0].icon}.png`,
+//         date: new Date(current.dt*1000),
+//         wind: current.wind.speed,
+//     }
+//     results.push(weatherOBJ);
+// }
+// console.log(results);
+// return results;
+// }; 
+
+const renderTodayWeather = (weatherOBJ) => {
+const h2 = document.querySelector(".current-h2");
+const details = document.querySelector(".current-details");
+const htmlString = `<img src=${weatherOBJ.icon} alt=${weatherOBJ.city}>
+<div>
+  <p>Temperature: ${weatherOBJ.temp}</p>
+  <p>Precipitation: ${weatherOBJ.precipitation}</p>
+  <p>Wind Speed: ${weatherOBJ.wind}</p>
+</div>`;
+
+h2.innerText = weatherOBJ.city;
+details.innerHTML = htmlString;
+};
+
+const h2 = document.querySelector(".current-h2");
+console.log(h2, "test h2");
 
 const fetchTodayWeather = () => {
     
@@ -44,15 +69,20 @@ const fetchTodayWeather = () => {
     })
     .then(res => res.json())
     .then(data => {
-        console.log(data)
+        
+        renderTodayWeather(data);
+        
     })
     
 
 };
 
+
+
 const submitSearch = () => {
 // fetchFiveDay();
 fetchTodayWeather();
+fetchFiveDay();
 };
 
 // event listners
--- WeatherApp/server.js
+++ WeatherApp/server.js
@@ -25,11 +25,41 @@ app.post('/search', (req, res) => {
             icon: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
             date: new Date(data.dt*1000),
             wind: data.wind.speed,
+            city: data.name,
         }
         console.log(weatherOBJ);
         res.status(201).json(weatherOBJ);
     })
     .catch(err => res.status(500).json(err));
+});
+
+const formatData = (weatherArray) => {
+  const results = [];
+  for (let i = 4; i < weatherArray.length; i+=8){
+      const current = weatherArray[i];
+      const weatherOBJ = {
+          temp: current.main.temp,
+          precipitation: current.weather[0].description,
+          icon: `http://openweathermap.org/img/w/${current.weather[0].icon}.png`,
+          date: new Date(current.dt*1000),
+          wind: current.wind.speed,
+      }
+      results.push(weatherOBJ);
+  }
+  console.log(results);
+  return results;
+}; 
+
+app.post('/fiveday', (req,res) => {
+  axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${req.body.city}&units=imperial&appid=${process.env.API_key}`)
+  
+  .then(response => {
+      console.log(response);
+     const results = formatData(response.data.list);
+     res.status(201).json(results);
+  })
+  .catch(err => res.status(500).json(err));
+
 });
 
 app.listen(port, () => {
